ab545f4a421fa50c1bd40a57ce14e75c4084f05f
Pedro Ferreira <pedroferreira@vialink.com.br>
2013-04-08 20:51:13 -0300

    incomplete setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='vlk-django-jsonfield',
+    version='1.0',
+    description='A model JSONField with an integrated form for django.',
+    url='https://github.com/vialink/vlk-jsonfield',
+)
