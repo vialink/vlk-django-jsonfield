3aa9d65fc59ee724a63adc2393da7639206535cf
Marcelo Salhab Brogliato <msbrogli@vialink.com.br>
2013-12-17 16:27:33 -0200

    Bumping up version to 1.2.1 and updating Github website

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='vlk-django-jsonfield',
-    version='1.1',
+    version='1.2.1',
     license='MIT',
     author='See README.md',
     description='A model JSONField with an integrated form for django.',
     long_description=open('README.md').read(),
     packages=['vlkjsonfield'],
-    url='https://github.com/vialink/vlk-jsonfield',
+    url='https://github.com/vialink/vlk-django-jsonfield',
 )
